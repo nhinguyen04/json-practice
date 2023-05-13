05a23a672d088cc3faeef3d42ce54cd9b4ebde90
nhinguyen04 <valentinetwin94@gmail.com>
2023-05-13 11:39:36 -0700

    complete 08-music-archive-server

--- practice-for-week-08-music-archive-server-long-practice/server.js
+++ practice-for-week-08-music-archive-server-long-practice/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const { url } = require('inspector');
 
 /* ============================ SERVER DATA ============================ */
 let artists = JSON.parse(fs.readFileSync('./seeds/artists.json'));
@@ -68,6 +69,92 @@ const server = http.createServer((req, res) => {
     /* ========================== ROUTE HANDLERS ========================== */
 
     // Your code here
+    const urlParts = req.url.split('/');
+    const deleteMessage = {
+      message: "Successfully deleted"
+    }
+
+    if (req.method === 'GET') {
+      // get all artists
+      if (req.url === '/artists') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify(artists));
+      }
+
+      // get artist details from artistId
+      if (req.url.startsWith('/artists/') && urlParts.length === 3) {
+        const artistId = urlParts[2];
+        const artist = artists[artistId];
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify(artist));
+      }
+
+      // get all album from artist based on artistId
+      if (req.url.endsWith('albums') && urlParts.length === 4) {
+        const artistId = urlParts[2];
+        let artistAlbums = {};
+
+        for (id in albums) {
+          let album = albums[id];
+
+          if (album.artistId === Number(artistId)) {
+            artistAlbums[id] = album;
+          }
+        }
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify(artistAlbums));
+      }
+    }
+
+    if (req.method === 'POST') {
+      // add an artist
+      if (req.url === '/artists/') {
+        const newId = getNewArtistId();
+
+        artists[newId] = {
+          artistId: newId,
+          ...req.body
+        }
+
+        res.statusCode = 201;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify(artists[newId]));
+      }
+    }
+
+    if (req.method === 'PATCH') {
+      // edit artist by artistId
+      if (req.url.startsWith('/artists/') && urlParts.length === 3) {
+        const artistId = urlParts[2];
+
+        artists[artistId] = {
+          artistId: Number(artistId),
+          ...req.body
+        }
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify(artists[artistId]));
+      }
+    }
+
+    if (req.method === 'DELETE') {
+      // delete artist by artistId
+      if (req.url.startsWith('/artists/') && urlParts.length === 3) {
+        const artistId = urlParts[2];
+        delete artists[artistId];
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify(deleteMessage));
+      }
+    }
+
 
     res.statusCode = 404;
     res.setHeader('Content-Type', 'application/json');
